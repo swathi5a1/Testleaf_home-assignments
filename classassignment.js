d643c22824729f8cb119064ba8b2775afb9771d2
swathi5a1 <96672910+swathi5a1@users.noreply.github.com>
2026-02-01 06:18:51 +0530

    Add files via upload

---
+++ classassignment.js
@@ -0,0 +1,11 @@
+let firstName = "Swathi";
+let companyName = "capgemini"
+let mobileNumber = 9994593289;
+let isAutoamtion = true;
+let lasPlaywright;
+
+console.log("the firstname is " +firstName, "the variable type is " +typeof(firstName));
+console.log("the companyName is " +companyName, "the variable type is " +typeof(companyName));
+console.log("the mobileNumber is " +mobileNumber, "the variable type is " +typeof(mobileNumber));
+console.log("the isAutoamtion is " +isAutoamtion, "the variable type is " +typeof(isAutoamtion));
+console.log("the lasPlaywright is " +lasPlaywright, "the variable type is " +typeof(lasPlaywright));
